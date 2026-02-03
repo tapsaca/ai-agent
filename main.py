5c6964944fb5867e3b1c2cbbecebdce71feb90e7
tapsaca <98756983+tapsaca@users.noreply.github.com>
2026-02-03 23:32:55 +0200

    Print token usage

--- main.py
+++ main.py
@@ -10,6 +10,11 @@ def main():
     
     client = genai.Client(api_key=api_key)
     response = client.models.generate_content(model="gemini-2.5-flash", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+    if not response.usage_metadata:
+        raise RuntimeError("Gemini API response appears to be malformed")
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    print("Response:")
     print(response.text)
 
 if __name__ == "__main__":
