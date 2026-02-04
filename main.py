f5c3a3650c2c2079a8a058898ad5a5577671432f
tapsaca <98756983+tapsaca@users.noreply.github.com>
2026-02-04 23:06:38 +0200

    Take user input from command line

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
+import argparse
 import os
 from dotenv import load_dotenv
 from google import genai
 
 def main():
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    args = parser.parse_args()
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     if not api_key:
         raise RuntimeError("GEMINI_API_KEY environment variable not set")
-    
     client = genai.Client(api_key=api_key)
-    response = client.models.generate_content(model="gemini-2.5-flash", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+    response = client.models.generate_content(model="gemini-2.5-flash", contents=args.user_prompt)
     if not response.usage_metadata:
         raise RuntimeError("Gemini API response appears to be malformed")
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
